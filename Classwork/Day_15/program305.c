d45eaaab516678f2336a16436d18afa2dd3a705f
AshwiniKauthale <kauthaleashwini40@gmail.com>
2026-01-03 22:14:40 +0530

    Add files via upload

--- Classwork/Day_15/program305.c
+++ Classwork/Day_15/program305.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 
+#pragma pack(1)
 struct node
 {
     int data;
     struct node *next;
 };
 
+typedef struct node NODE;
+
+
 int main()
 {
-    struct node obj;
+    // struct node obj;
+
+    NODE obj;
 
     printf("%ld",sizeof(obj));
 
